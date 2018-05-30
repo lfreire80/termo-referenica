77f24a7d9ab39177b85f26b19ab33b8d10bd26f1
Leonardo Freire <lfreire80@gmail.com>
2018-05-30 13:56:40 -0300

    teste de unidade do componente TermoReferenciaList

--- src/components/TermoReferenciaList.spec.js
+++ src/components/TermoReferenciaList.spec.js
@@ -42,19 +42,26 @@ describe('TermoReferenciaList', () =>{
 
     test('Verifica se os tipos estão corretos conforme arquivo de Tipos', () => {
         expect(Tipos).toEqual(wrapper.vm.Tipos);
-    });
+    })
 
     test('Verifica se os status estão corretos conforme aquivo de Status', () => {
         expect(Status).toEqual(wrapper.vm.Status);
-    });
+    })
 
     test('Estado inicial do filtro deve ser false', () => {
         expect(wrapper.vm.filtroHabilitado).toBeFalsy()
     })
     
-    test('mudaStatusTermo deve modificar o status do termo conforme parametros', () => {
+    test('MudaStatusTermo deve modificar o status do termo conforme parametros', () => {
         const lastTermo = wrapper.vm.termos[wrapper.vm.termos.length - 1]
         wrapper.vm.mudaStatusTermo(lastTermo.numero, 4)
         expect(4).toEqual(lastTermo.status);
-    });
+    })
+
+    test('Método baixar() deve chamar a action baixaTermo ', () => {
+        const baixaTermoStub = sinon.stub()
+        wrapper.vm.baixaTermo = baixaTermoStub;
+        wrapper.vm.baixar(0)
+        expect(baixaTermoStub.called).toBe(true)
+    })
 })
