97cb1a5af97cbac20ee41a4377d536bfff4e4bcb
Leonardo Freire <lfreire80@gmail.com>
2018-06-11 17:05:21 -0300

    exclusão de arquivos não utilizados

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use scrict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    ROOT_API: '"http://localhost:49894/api/"' 
-})
--- config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-    NODE_ENV: '"production"',
-    ROOT_API: '"https://portal.fujb.ufrj.br/FujbPortal.API/api/"'
-}
